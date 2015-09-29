f434921a285711ba5caa3bd85f48a4b68bbf731a
moocowmoo <moocowmoo@masternode.me>
2015-09-29 18:02:37 +0000

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dash-rpctest
 simple up/down curl test for dashd responsiveness
+
+dash-rpctest.sh will exit with a non-zero status if dashd rpc does not respond.
+
+# usage
+
+in a script or cron, etc., do:
+
+/path/to/dash-rpctest.sh || dashd_is_unresponsive_do_something_clever
+
---
+++ dash-rpctest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DASH_CONF=$HOME/.dash/dash.conf
+
+RPCUSER=$( grep rpcuser $DASH_CONF | sed -e 's/=/ /' | awk '{print $2}')
+RPCPASS=$( grep rpcpassword  $DASH_CONF | sed -e 's/=/ /' | awk '{print $2}')
+
+CURL=`which curl` || (echo "depenency curl missing. do: sudo apt-get install curl " && exit 1)
+
+response=$( $CURL -qq --user $RPCUSER:$RPCPASS --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:9998/ 2>/dev/null )
+
+if [[ "$response" == *"protocolversion"*  ]] ; then
+    exit 0
+fi
+
+exit 1
