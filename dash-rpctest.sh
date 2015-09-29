eea41305a37acef66e50acf54c8c370844f07544
moocowmoo <moocowmoo@masternode.me>
2015-09-29 18:41:16 +0000

    set curl timeout to ten seconds

--- dash-rpctest.sh
+++ dash-rpctest.sh
@@ -7,7 +7,7 @@ RPCPASS=$( grep rpcpassword  $DASH_CONF | sed -e 's/=/ /' | awk '{print $2}')
 
 CURL=`which curl` || (echo "depenency curl missing. do: sudo apt-get install curl " && exit 1)
 
-response=$( $CURL -qq --user $RPCUSER:$RPCPASS --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:9998/ 2>/dev/null )
+response=$( $CURL -qq --user $RPCUSER:$RPCPASS --max-time 10 --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:9998/ 2>/dev/null )
 
 if [[ "$response" == *"protocolversion"*  ]] ; then
     exit 0
